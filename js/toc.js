23263df7b1325226e989fc7996c460708069518b
PingHuaZhuang <zphua2016@webmail.com>
2017-09-03 14:43:27 +0800

    Site updated: 2017-09-03 14:43:26

--- js/toc.js
+++ js/toc.js
@@ -1,18 +1,1 @@
-var toc = document.getElementById('toc')
-
-if (toc != null) {
-	window.addEventListener("scroll", scrollcatelogHandler);
-	var tocPosition = toc.offsetTop;
-	var height_header = $("#signature").height();
-	function scrollcatelogHandler(e) {
-		 var event = e || window.event,
-		     target = event.target || event.srcElement;
-		 var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		 if (scrollTop >  tocPosition -60) {
-		     toc.classList.add("toc-fixed");
-		 } else {
-		     toc.classList.remove("toc-fixed");
-		 }
-	}
-}
-
+var toc=document.getElementById("toc");if(null!=toc){window.addEventListener("scroll",scrollcatelogHandler);var tocPosition=toc.offsetTop,height_header=$("#signature").height();function scrollcatelogHandler(o){var t=o||window.event;t.target||t.srcElement;(document.documentElement.scrollTop||document.body.scrollTop)>tocPosition-60?toc.classList.add("toc-fixed"):toc.classList.remove("toc-fixed")}}
--- js/totop.js
+++ js/totop.js
@@ -1,13 +1,1 @@
-$(window).scroll(function() {
-    $(window).scrollTop() > $(window).height()*0.5 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
-});
-
-$("#rocket").click(function() {
-    $("#rocket").addClass("launch");
-    $("html, body").animate({
-        scrollTop: 0
-    }, 1000, function() {
-        $("#rocket").removeClass("show launch");
-    });
-    return false;
-});
+$(window).scroll(function(){$(window).scrollTop()>.5*$(window).height()?$("#rocket").addClass("show"):$("#rocket").removeClass("show")}),$("#rocket").click(function(){return $("#rocket").addClass("launch"),$("html, body").animate({scrollTop:0},1e3,function(){$("#rocket").removeClass("show launch")}),!1});
